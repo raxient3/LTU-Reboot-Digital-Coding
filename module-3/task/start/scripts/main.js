cf9ff38a6068f2f4899d6320879284b4fd262518
raxient3 <raxient3@gmail.com>
2021-05-20 19:24:43 +0100

    images added

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,20 @@
+console.log('Hello');
+
+// single line comment
+
+/*
+Multiple line comment
+
+*/
+
+var productName = "Tinned beans";
+let price = "0.40";
+const quantity = 1;
+var inStock = true;
+let discountAmount = 0;
+
+console.log(productName);
+console.log(price);
+console.log(quantity);
+console.log(inStock);
+console.log(discountAmount);
